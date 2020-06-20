498cf704bb94518a02f41b20871e54a99b29ade0
Chris Daniel <poputeacristi@gmail.com>
2020-06-20 20:03:55 +0300

    add more steps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { useReactToPrint } from 'react-to-print';
-import { Button } from 'react-bootstrap';
+import { Button, FormFile } from 'react-bootstrap';
 import './main.scss';
 import TablePrint from "./TablePrint";
 
@@ -25,7 +25,7 @@ function App() {
     const content = fileReader.result;
     const tableData = [];
     content.split('\n').forEach((line) => {
-      if(line) {
+      if (line) {
         let attributes = line.split(';');
         const heightIsSmaller = parseFloat(attributes[HEIGHT_INDEX]) < parseFloat(attributes[WIDTH_INDEX]);
         let edgeText = '';
@@ -65,11 +65,34 @@ function App() {
   };
   return (
     <div className="p-3">
-      <h1>Upload polyboard ascii export</h1>
-      <input type="text" value="<material>;<quantity>;<height>;<width>;<top_edge_present>;" readOnly className="w-100"/><br/>
-      <input type="file" onChange={event => handleFileChosen(event.target.files[0])} accept=".txt" /><br />
-      <Button onClick={handlePrint} className="mt-4">Printeaza table</Button><br />
-      {(table && table.length) ? <TablePrint table={table} ref={componentRef} /> : 'Incarca fisier'}
+      <h1>Exporta lista mobila formated according to Petre Jarda 👷🏻‍♂️🪑💪</h1>
+      <h3>Pasul 1</h3>
+      <p>Copiaza urmatorul text in clipboard.</p>
+      <input type="text" value="<material>;<quantity>;<height>;<width>;<top_edge_present>;" readOnly
+             className="w-100" onClick={(event)=>{
+               event.target.focus();event.target.select();
+      }}/><br />
+      <h3>Pasul 2</h3>
+      <p>Lipseste/Paste textul de mai sus conform imagini de mai jos</p>
+      <img />
+      <h3>Pasul 3</h3>
+      <p>Bifeza optiunea cu net dimension ca si in imaginea de mai sus si asigurate ca toate configuriatiile corespund
+      cu imaginea de mai sus</p>
+      <h3>Pasul 4</h3>
+      <p>Apasa pe ASCII export conform imagini de mai jos si decarca fisierul txt</p>
+      <h3>Pasul 5</h3>
+      <p>Adauga fisierul descarcat in casuta de mai jos</p>
+      <h3>Pasul final printeaza tabelul apasand pe butonul de mai jos</h3>
+      <FormFile
+        id="custom-file"
+        label="Incarca fisierul txt"
+        custom
+        data-browse="Incarca"
+        className="w-50"
+        onChange={event => handleFileChosen(event.target.files[0])} accept=".txt"
+      /><br />
+      <Button onClick={handlePrint} className="mt-4">Printeaza tabel</Button><br />
+      {(table && table.length) ? <TablePrint table={table} ref={componentRef} />:'Nu ai incarcat niciun fisier'}
     </div>
   );
 }
